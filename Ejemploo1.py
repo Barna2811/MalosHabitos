fcb2c0673f2bb52969aeb25194bb9b3cc83d8fc5
Barna2811 <72771058@continental.edu.pe>
2024-04-10 16:53:43 -0500

    Verificar malos habitos

---
+++ Ejemploo1.py
@@ -0,0 +1,11 @@
+x = 10
+y = 5
+z = x + y
+
+def multiplicar(a, b):
+    return a * b
+
+resultado = multiplicar(x, z)
+
+
+print("El resultado es:", resultado)
---
+++ Ejemploo2.py
@@ -0,0 +1,8 @@
+def calcular(a, b, c):
+    return a * b + c
+
+def principal():
+    resultado = calcular(5, 3, 7)
+    print("El resultado es:", resultado)
+
+principal()
